a87a6a373d06b3ba698cb09b1ca199c92bf65ebc
Tom R <137434709+mr-tomr@users.noreply.github.com>
2024-05-08 12:17:54 -0400

    Update DisplayGroupOfFindings.sh

--- DisplayGroupOfFindings.sh
+++ DisplayGroupOfFindings.sh
@@ -6,12 +6,12 @@
 # Used when scanning a large number of ports on one host.
 
 
-# OneLiner - for file in *; do if [[ $(wc -l < "$file") -gt 0 ]]; then echo "File: $file"; cat "$file"; echo; fi; done
+# OneLiner - for file in *.txt; do if [[ $(wc -l < "$file") -gt 0 ]]; then echo "File: $file"; cat "$file"; echo; fi; done
 
 #!/bin/bash
  
 # Loop through all files in the current directory
-for file in *; do
+for file in *.txt; do
     # Check if wc output for the file is greater than 0
     if [[ $(wc -l < "$file") -gt 0 ]]; then
         # Echo the filename
@@ -21,3 +21,8 @@ for file in *; do
         echo  # Add an empty line for separation
     fi
 done
+
+
+# This will need to be filtered to create Instances in a report
+# output results then grab the host names with IPs
+./catresults.sh | grep Host | awk '{print $4}' | sort -t '_' -k 2n | sed 's/\.txt$//'
